59957412f2801abccbe050167ff52e724f0f6cd1
Timothy Sewell <sewell.tim@gmail.com>
2024-12-20 08:33:37 +0000

    Fixed typo

--- src/api/hooks/saveSubmittedFunds.ts
+++ src/api/hooks/saveSubmittedFunds.ts
@@ -4,7 +4,10 @@ export const useSaveSubmittedFunds = (user: User) => {
   const saveFunds = (funds: Fund[], multiple: boolean) => {
     const key = multiple ? 'multiple' + user.id : String(user.id);
 
-    localStorage.setItem(key, JSON.stringify(funds.filter(({ amount }) => amount && amount > 0));
+    localStorage.setItem(
+      key,
+      JSON.stringify(funds.filter(({ amount }) => amount && amount > 0))
+    );
   };
 
   return { saveFunds };
